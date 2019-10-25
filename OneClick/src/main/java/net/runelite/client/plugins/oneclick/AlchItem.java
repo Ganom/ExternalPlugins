452d93e5c83e4f269fd93a145f2007164ba7e397
Ganom <theariall@gmail.com>
2019-10-25 01:57:55 -0400

    license: add GPL3 License.

--- OneClick/src/main/java/net/runelite/client/plugins/oneclick/AlchItem.java
+++ OneClick/src/main/java/net/runelite/client/plugins/oneclick/AlchItem.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, ganom <https://github.com/Ganom>
+ * Copyright (c) 2019, TomC <https://github.com/tomcylke>
+ * All rights reserved.
+ * Licensed under GPL3, see LICENSE for the full scope.
+ */
 package net.runelite.client.plugins.oneclick;
 
 import lombok.Getter;
