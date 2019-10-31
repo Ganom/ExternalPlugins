1d566acdcddb7e01120b83326130742dca69bb15
Ganom <theariall@gmail.com>
2019-10-31 16:02:48 -0400

    oneclick: update modified event. leftclickpk: update modified event.

--- OneClick/src/main/java/net/runelite/client/plugins/oneclick/OneClickPlugin.java
+++ OneClick/src/main/java/net/runelite/client/plugins/oneclick/OneClickPlugin.java
@@ -273,7 +273,7 @@ public class OneClickPlugin extends Plugin
 			setHighAlchItem.setForceLeftClick(false);
 			menuList[1] = setHighAlchItem;
 			event.setMenuEntries(menuList);
-			event.setModified(true);
+			event.setModified();
 		}
 	}
 
@@ -294,7 +294,7 @@ public class OneClickPlugin extends Plugin
 						return;
 					}
 					event.setTarget("<col=ff9040>Feather<col=ffffff> -> " + targetMap.get(id));
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case FIREMAKING:
@@ -305,7 +305,7 @@ public class OneClickPlugin extends Plugin
 						return;
 					}
 					event.setTarget("<col=ff9040>Tinderbox<col=ffffff> -> " + targetMap.get(id));
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case BIRDHOUSES:
@@ -318,7 +318,7 @@ public class OneClickPlugin extends Plugin
 					event.setOption("Use");
 					event.setTarget("<col=ff9040>Hops seed<col=ffffff> -> " + targetMap.get(id));
 					event.setOpcode(MenuOpcode.ITEM_USE_ON_GAME_OBJECT.getId());
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case HERB_TAR:
@@ -329,7 +329,7 @@ public class OneClickPlugin extends Plugin
 						return;
 					}
 					event.setTarget("<col=ff9040>Swamp tar<col=ffffff> -> " + targetMap.get(id));
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case LAVA_RUNES:
@@ -348,7 +348,7 @@ public class OneClickPlugin extends Plugin
 						}
 						event.setOption("Empty");
 						event.setTarget("<col=ff9040>Pouches");
-						event.setModified(true);
+						event.setModified();
 						return;
 					}
 
@@ -356,12 +356,12 @@ public class OneClickPlugin extends Plugin
 					{
 						event.setOption("Use");
 						event.setTarget("<col=ff9040>Magic Imbue<col=ffffff> -> <col=ffff>Yourself");
-						event.setModified(true);
+						event.setModified();
 						return;
 					}
 					event.setOption("Use");
 					event.setTarget("<col=ff9040>Earth rune<col=ffffff> -> <col=ffff>Altar");
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case HIGH_ALCH:
@@ -369,7 +369,7 @@ public class OneClickPlugin extends Plugin
 				{
 					event.setOption("Cast");
 					event.setTarget("<col=00ff00>High Level Alchemy</col><col=ffffff> -> " + alchItem.getName());
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case DWARF_CANNON:
@@ -381,7 +381,7 @@ public class OneClickPlugin extends Plugin
 					}
 					event.setOption("Use");
 					event.setTarget("<col=ff9040>Cannonball<col=ffffff> -> <col=ffff>Dwarf multicannon");
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case BONES:
@@ -393,7 +393,7 @@ public class OneClickPlugin extends Plugin
 					}
 					event.setOption("Use");
 					event.setTarget("<col=ff9040>Bones<col=ffffff> -> " + event.getTarget());
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case KARAMBWANS:
@@ -405,7 +405,7 @@ public class OneClickPlugin extends Plugin
 					}
 					event.setOption("Use");
 					event.setTarget("<col=ff9040>Raw karambwan<col=ffffff> -> " + event.getTarget());
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 			case DARK_ESSENCE:
@@ -416,7 +416,7 @@ public class OneClickPlugin extends Plugin
 						return;
 					}
 					event.setTarget("<col=ff9040>Chisel<col=ffffff> -> <col=ff9040>Dark essence block");
-					event.setModified(true);
+					event.setModified();
 				}
 				break;
 		}
