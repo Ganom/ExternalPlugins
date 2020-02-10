fb86bde3d32e0ba063b8e9ec8b387fd409274fdc
Ganom <theariall@gmail.com>
2020-02-09 19:41:54 -0500

    project: actually load extutils as a plugin dep.

--- ExtUtils/extutils.gradle.kts
+++ ExtUtils/extutils.gradle.kts
@@ -28,6 +28,9 @@ import ProjectVersions.rlVersion
 
 version = rlVersion
 
+project.extra["PluginName"] = "ExtUtils"
+project.extra["PluginDescription"] = "Utils required for plugins to function."
+
 dependencies {
     annotationProcessor(Libraries.lombok)
     annotationProcessor(Libraries.pf4j)
@@ -41,4 +44,18 @@ dependencies {
     implementation(Libraries.okhttp3)
     implementation(Libraries.apacheCommonsText)
     implementation(Libraries.rxjava)
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
+    }
 }
