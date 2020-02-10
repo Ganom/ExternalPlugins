073398cf677d60ab2feed1361f5acc06f1550da6
Ganom <theariall@gmail.com>
2020-02-10 02:52:49 -0500

    project: fix classnotfound error.

--- ItemDropper/itemdropper.gradle.kts
+++ ItemDropper/itemdropper.gradle.kts
@@ -52,6 +52,7 @@ tasks {
                     "Plugin-Version" to project.version,
                     "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                     "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Dependencies" to nameToId("extutils"),
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]
             ))
--- OlmSwapper/olmswapper.gradle.kts
+++ OlmSwapper/olmswapper.gradle.kts
@@ -53,6 +53,7 @@ tasks {
                     "Plugin-Version" to project.version,
                     "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                     "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Dependencies" to nameToId("extutils"),
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]
             ))
