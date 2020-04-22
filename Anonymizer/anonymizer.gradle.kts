32539926c28c80c3fb5bf20651fcb50765e04663
Ganom <theariall@gmail.com>
2020-04-22 06:48:26 -0400

    anonymizer: deploy new artifact.

--- Anonymizer/anonymizer.gradle.kts
+++ Anonymizer/anonymizer.gradle.kts
@@ -24,7 +24,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.10.0"
+version = "5.11.0"
 
 project.extra["PluginName"] = "Anonymizer"
 project.extra["PluginDescription"] = "Anonymizes your rsn and others."
