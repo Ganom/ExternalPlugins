e8be316e57c469ddeefd14ff4583ec5f0c1e5536
fakegamer42 <77029161+fakegamer42@users.noreply.github.com>
2021-01-06 15:49:31 -0500

    Trouble brew one click
    Noob attempt at making using buckets of water on hopper 1 click

--- OneClick/src/main/java/net/runelite/client/plugins/externals/oneclick/Comparables/skilling/Tiara.java
+++ OneClick/src/main/java/net/runelite/client/plugins/externals/oneclick/Comparables/skilling/Tiara.java
@@ -5,26 +5,26 @@ import net.runelite.api.MenuEntry;
 import net.runelite.api.MenuOpcode;
 import net.runelite.client.plugins.externals.oneclick.comparables.ClickCompare;
 
-public class Tiara extends ClickCompare
+public class TB extends ClickCompare
 {
 	@Override
 	public boolean isEntryValid(MenuEntry event)
 	{
 		return event.getOpcode() == MenuOpcode.GAME_OBJECT_FIRST_OPTION.getId() &&
-			event.getOption().equals("Craft-rune") &&
-			event.getTarget().equals("<col=ffff>Altar");
+			event.getOption().equals("Use") &&
+			event.getTarget().equals("<col=ffff>Hopper");
 	}
 
 	@Override
 	public void modifyEntry(MenuEntry event)
 	{
-		if (findItem(ItemID.TIARA).getLeft() == -1)
+		if (findItem(ItemID.BUCKET-OF-WATER).getLeft() == -1)
 		{
 			return;
 		}
 		MenuEntry e = event.clone();
 		e.setOption("Use");
-		e.setTarget("<col=ff9040>Tiara<col=ffffff> -> <col=ffff>Altar");
+		e.setTarget("<col=ff9040>Bucket-of-water<col=ffffff> -> <col=ffff>Hopper");
 		e.setForceLeftClick(true);
 		insert(e);
 	}
@@ -33,13 +33,13 @@ public class Tiara extends ClickCompare
 	public boolean isClickValid(MenuEntry event)
 	{
 		return event.getOpcode() == MenuOpcode.GAME_OBJECT_FIRST_OPTION.getId() &&
-			event.getTarget().equals("<col=ff9040>Tiara<col=ffffff> -> <col=ffff>Altar");
+			event.getTarget().equals("<col=ff9040>Bucket-of-water<col=ffffff> -> <col=ffff>Hopper");
 	}
 
 	@Override
 	public void modifyClick(MenuEntry event)
 	{
-		if (updateSelectedItem(ItemID.TIARA))
+		if (updateSelectedItem(ItemID.BUCKET-OF-WATER))
 		{
 			event.setOpcode(MenuOpcode.ITEM_USE_ON_GAME_OBJECT.getId());
 		}
