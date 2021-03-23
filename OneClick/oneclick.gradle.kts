216ddfaf34184b17b64d6665db97751e1663ffa8
thhdragon <46762155+thhdragon@users.noreply.github.com>
2021-03-22 22:04:23 -0400

    oneclick: update ver

--- OneClick/oneclick.gradle.kts
+++ OneClick/oneclick.gradle.kts
@@ -24,7 +24,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "6.0.2"
+version = "6.0.3"
 
 project.extra["PluginName"] = "OneClick"
 project.extra["PluginDescription"] = "OP One Click methods."
