f0b79f1ae1b7b21a8fa97b8e10960ebff648fe34
Ganom <ganom@ganomsoftware.com>
2022-04-25 23:53:09 -0400

    oneclick: fix methods not being enabled on config toggle.

--- OneClick/oneclick.gradle.kts
+++ OneClick/oneclick.gradle.kts
@@ -24,7 +24,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "8.0.3"
+version = "8.0.4"
 
 project.extra["PluginName"] = "OneClick"
 project.extra["PluginDescription"] = "OP One Click methods."
