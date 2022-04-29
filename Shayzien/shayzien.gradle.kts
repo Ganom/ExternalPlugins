52074b9bafe6c0894852610701be5e9e0e613cdd
Ganom <ganom@ganomsoftware.com>
2022-04-29 06:25:58 -0400

    project: test bootstrap.

--- Shayzien/shayzien.gradle.kts
+++ Shayzien/shayzien.gradle.kts
@@ -24,7 +24,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.0.0"
+version = "1.0.1"
 
 project.extra["PluginName"] = "Shayzien Assist"
 project.extra["PluginDescription"] = "Because this favor is trash."
